79ca82c11027388f6183cab44c901edff14cc15a
Felipe Cepluki <felipecepluki@gmail.com>
2023-04-06 22:31:10 -0300

    Adding functionality: creating login and improving home

---
+++ Financas/frontend-financas/src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Container, Title, ButtonMenu } from "./styles";
+import Feather from "@expo/vector-icons/Feather";
+import { useNavigation } from "@react-navigation/native";
+
+export default function Header({ title }) {
+  const navigation = useNavigation();
+
+  return (
+    <Container>
+      <ButtonMenu onPress={() => navigation.openDrawer()}>
+        <Feather name="menu" size={35} color="#121212" />
+      </ButtonMenu>
+      {title && <Title>{title}</Title>}
+    </Container>
+  );
+}
--- Financas/frontend-financas/src/pages/Home/index.js
+++ Financas/frontend-financas/src/pages/Home/index.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { AuthContext } from "../../contexts/auth";
+import { Background } from "./styles";
+import Header from "../../components/Header";
 
 export default function Home() {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <Background>
+      <Header title="Minhas movimentações" />
+    </Background>
   );
 }
