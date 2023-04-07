13680398a64e01776503a97453d784671d4f4e07
Felipe Cepluki Lopes <felipecepluki@pop-os.localdomain>
2023-04-06 21:52:59 -0300

    Add features: logging user and keeping login

--- Financas/frontend-financas/src/contexts/auth.js
+++ Financas/frontend-financas/src/contexts/auth.js
@@ -1,13 +1,37 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import api from "../services/api";
 import { useNavigation } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [loadingAuth, setLoadingAuth] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const navigation = useNavigation();
+
+  useEffect(() => {
+    async function loadStorage() {
+      const storageUser = await AsyncStorage.getItem("@finToken");
+      if (storageUser) {
+        const response = await api
+          .get("/me", {
+            headers: {
+              Authorization: `Bearer ${storageUser}`,
+            },
+          })
+          .catch(() => {
+            setUser(null);
+          });
+        api.defaults.headers["Authorization"] = `Bearer ${storageUser}`;
+        setUser(response.data);
+        setLoading(false);
+      }
+      setLoading(false);
+    }
+    loadStorage();
+  }, []);
 
   async function signUp(name, email, password) {
     setLoadingAuth(true);
@@ -25,8 +49,38 @@ function AuthProvider({ children }) {
     }
   }
 
+  async function signIn(email, password) {
+    setLoadingAuth(true);
+    try {
+      const response = await api.post("/login", {
+        email: email,
+        password: password,
+      });
+      const { id, name, token } = response.data;
+      const data = {
+        id,
+        name,
+        token,
+        email,
+      };
+      await AsyncStorage.setItem("@finToken", token);
+      api.defaults.headers["Authorization"] = `Bearer ${token}`;
+      setUser({
+        id,
+        name,
+        email,
+      });
+      setLoadingAuth(false);
+    } catch (err) {
+      console.log("Erro ao logar", err);
+      setLoadingAuth(false);
+    }
+  }
+
   return (
-    <AuthContext.Provider value={{ signed: !!user, user, signUp, loadingAuth }}>
+    <AuthContext.Provider
+      value={{ signed: !!user, user, signUp, signIn, loadingAuth, loading }}
+    >
       {children}
     </AuthContext.Provider>
   );
--- Financas/frontend-financas/src/routes/index.js
+++ Financas/frontend-financas/src/routes/index.js
@@ -1,12 +1,26 @@
 import React, { useContext } from "react";
-import { View, ActiveIndicator } from "react-native";
+import { View, ActivityIndicator } from "react-native";
 import { AuthContext } from "../contexts/auth";
 import AuthRoutes from "./auth.routes";
 import AppRoutes from "./app.routes";
 
 function Routes() {
-  const { signed } = useContext(AuthContext);
-  const loading = false;
+  const { signed, loading } = useContext(AuthContext);
+
+  if (loading) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#F0F4FF",
+        }}
+      >
+        <ActivityIndicator size="large" color="#131313" />
+      </View>
+    );
+  }
 
   return signed ? <AppRoutes /> : <AuthRoutes />;
 }
