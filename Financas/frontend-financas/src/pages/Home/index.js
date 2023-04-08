3cfe5405db07628a69484a0f79152aa741ba5e99
Felipe Cepluki <felipecepluki@gmail.com>
2023-04-08 13:19:17 -0300

    Adding functionality: daily cards

--- Financas/frontend-financas/src/pages/Home/index.js
+++ Financas/frontend-financas/src/pages/Home/index.js
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../contexts/auth";
-import { Background } from "./styles";
+import { Background, ListBalance } from "./styles";
 import Header from "../../components/Header";
+import api from "../../services/api";
+import { format } from "date-fns";
+import { useIsFocused } from "@react-navigation/native";
+import BalanceItem from "../../components/BalanceItem";
 
 export default function Home() {
+  const isFocused = useIsFocused();
+  const [listBalance, setListBalance] = useState([]);
+  const [dateMovements, setDateMovements] = useState(new Date());
+
+  useEffect(() => {
+    let isActive = true;
+    async function getMovements() {
+      let dateFormated = format(dateMovements, "dd/MM/yyyy");
+      const balance = await api.get("/balance", {
+        params: {
+          date: dateFormated,
+        },
+      });
+      if (isActive) {
+        setListBalance(balance.data);
+      }
+    }
+    getMovements();
+    return () => (isActive = false);
+  }, [isFocused]);
+
   return (
     <Background>
       <Header title="Minhas movimentações" />
+      <ListBalance
+        data={listBalance}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item) => item.tag}
+        renderItem={({ item }) => <BalanceItem data={item} />}
+      />
     </Background>
   );
 }
