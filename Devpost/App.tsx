0cf8e028dd83922aa510aed98b43aaa53e91b948
Felipe Cepluki <felipecepluki@gmail.com>
2023-04-15 15:25:38 -0300

    Create Context

--- Devpost/App.tsx
+++ Devpost/App.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import Routes from './src/routes';
+import AuthProvider from './src/contexts/auth';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <StatusBar
-        backgroundColor="#36393F"
-        barStyle="light-content"
-        translucent={false}
-      />
-      <Routes />
+      <AuthProvider>
+        <StatusBar
+          backgroundColor="#36393F"
+          barStyle="light-content"
+          translucent={false}
+        />
+        <Routes />
+      </AuthProvider>
     </NavigationContainer>
   );
 }
