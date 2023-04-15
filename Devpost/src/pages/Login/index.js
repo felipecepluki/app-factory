4bb6b5b2108f70ef74a0774ffe3696da5e00756a
Felipe Cepluki <felipecepluki@gmail.com>
2023-04-15 15:13:58 -0300

    Create Login Page

--- Devpost/src/pages/Login/index.js
+++ Devpost/src/pages/Login/index.js
@@ -1,11 +1,95 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {Text} from 'react-native';
+import {
+  Container,
+  Title,
+  Input,
+  Button,
+  ButtonText,
+  SignUpButton,
+  SignUpText,
+} from './styles';
 
 function Login() {
+  const [login, setLogin] = useState(true);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function toggleLogin() {
+    setLogin(!login);
+    setName('');
+    setEmail('');
+    setPassword('');
+  }
+
+  function handleSignIn() {
+    if (email === '' || password === '') {
+      console.log('Preencha todos os campos!');
+      return;
+    }
+    // Fazer o login do user
+  }
+
+  function handleSignUp() {
+    if (name === '' || email === '' || password === '') {
+      console.log('Preencha todos os campos!');
+      return;
+    }
+    // Cadastrar o usuario na aplicacao
+  }
+
+  if (login) {
+    <Container>
+      <Title>
+        Dev<Text style={{color: '#E52246'}}>Post</Text>
+      </Title>
+      <Input
+        placeholder="seuemail@teste.com"
+        value={email}
+        onChangeText={text => setEmail(text)}
+      />
+      <Input
+        placeholder="******"
+        value={password}
+        onChangeText={text => setPassword(text)}
+      />
+      <Button onPress={handleSignIn}>
+        <ButtonText>Acessar</ButtonText>
+      </Button>
+      <SignUpButton onPress={toggleLogin}>
+        <SignUpText>Criar uma conta</SignUpText>
+      </SignUpButton>
+    </Container>;
+  }
+
   return (
-    <View>
-      <Text>Login</Text>
-    </View>
+    <Container>
+      <Title>
+        Dev<Text style={{color: '#E52246'}}>Post</Text>
+      </Title>
+      <Input
+        placeholder="Seu nome"
+        value={name}
+        onChangeText={text => setName(text)}
+      />
+      <Input
+        placeholder="seuemail@teste.com"
+        value={email}
+        onChangeText={text => setEmail(text)}
+      />
+      <Input
+        placeholder="******"
+        value={password}
+        onChangeText={text => setPassword(text)}
+      />
+      <Button onPress={handleSignUp}>
+        <ButtonText>Cadastrar</ButtonText>
+      </Button>
+      <SignUpButton onPress={toggleLogin}>
+        <SignUpText>Já tenho uma conta</SignUpText>
+      </SignUpButton>
+    </Container>
   );
 }
 
--- Devpost/src/routes/index.js
+++ Devpost/src/routes/index.js
@@ -9,15 +9,17 @@ function Routes() {
   const loading = false;
 
   if (loading) {
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#36393F',
-      }}>
-      <ActivityIndicator size={50} color="#E52246" />
-    </View>;
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#36393F',
+        }}>
+        <ActivityIndicator size={50} color="#E52246" />
+      </View>
+    );
   }
 
   return signed ? <AppRoutes /> : <AuthRoutes />;
