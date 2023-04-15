c2c299f1bf3cc4c406563203e5e424c280bbe38b
Felipe Cepluki <felipecepluki@gmail.com>
2023-04-15 14:41:42 -0300

    Create Routes in Devpost Project

---
+++ Devpost/src/pages/NewPost/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function NewPost() {
+  return (
+    <View>
+      <Text>NewPost</Text>
+    </View>
+  );
+}
+
+export default NewPost;
