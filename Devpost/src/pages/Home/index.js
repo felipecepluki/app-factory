739f49d232ba2309618c91054a166a0798c138d7
Felipe Cepluki <felipecepluki@gmail.com>
2023-04-17 05:08:32 -0300

    Adding DevPost functionality: New Post page

--- Devpost/src/pages/Home/index.js
+++ Devpost/src/pages/Home/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Text} from 'react-native';
+import {Container, ButtonPost} from './styles';
+import Feather from 'react-native-vector-icons/Feather';
+import {useNavigation} from '@react-navigation/native';
 
 function Home() {
+  const navigation = useNavigation();
+
   return (
-    <View>
+    <Container>
       <Text>Home</Text>
-    </View>
+      <ButtonPost
+        activeOpacity={0.8}
+        onPress={() => navigation.navigate('NewPost')}>
+        <Feather name="edit-2" color="#fff" size={25} />
+      </ButtonPost>
+    </Container>
   );
 }
 
--- Devpost/src/pages/NewPost/index.js
+++ Devpost/src/pages/NewPost/index.js
@@ -1,11 +1,33 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState, useLayoutEffect} from 'react';
+import {Container, Input, Button, ButtonText} from './styles';
+import {useNavigation} from '@react-navigation/native';
 
 function NewPost() {
+  const navigation = useNavigation();
+  const [post, setPost] = useState('');
+
+  useLayoutEffect(() => {
+    const options = navigation.setOptions({
+      headerRight: () => (
+        <Button>
+          <ButtonText>Compartilhar</ButtonText>
+        </Button>
+      ),
+    });
+  }, [navigation, post]);
+
   return (
-    <View>
-      <Text>NewPost</Text>
-    </View>
+    <Container>
+      <Input
+        placeholder="O que está acontecendo ?"
+        value={post}
+        onChangeText={text => setPost(text)}
+        autoCorrect={false}
+        multiline={true}
+        placeholderTextColor="#ddd"
+        maxLength={300}
+      />
+    </Container>
   );
 }
 
