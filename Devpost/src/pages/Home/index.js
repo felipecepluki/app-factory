ea4cafc87c29203dc94f43127d6117038a6ce36b
Felipe Cepluki <felipecepluki@gmail.com>
2023-04-17 15:49:37 -0300

    Adding DevPost functionality: Customizing Home and Header

--- Devpost/src/pages/Home/index.js
+++ Devpost/src/pages/Home/index.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text} from 'react-native';
-import {Container, ButtonPost} from './styles';
+import {Container, ButtonPost, ListPosts} from './styles';
 import Feather from 'react-native-vector-icons/Feather';
 import {useNavigation} from '@react-navigation/native';
+import Header from '../../components/Header';
 
 function Home() {
   const navigation = useNavigation();
+  const [posts, setPosts] = useState([
+    {id: '1', content: 'TESTE123'},
+    {id: '2', content: 'Segundo'},
+    {id: '3', content: 'Terceiro'},
+  ]);
 
   return (
     <Container>
-      <Text>Home</Text>
+      <Header />
+      <ListPosts data={posts} renderItem={({item}) => <Text>Teste</Text>} />
       <ButtonPost
         activeOpacity={0.8}
         onPress={() => navigation.navigate('NewPost')}>
