3a304893a3e5d61d44d1785573028cbc09875f7a
Felipe Cepluki <felipecepluki@gmail.com>
2023-04-17 04:37:49 -0300

    Adding DevPost functionality: configuring tabbar

--- Devpost/src/routes/app.routes.js
+++ Devpost/src/routes/app.routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Feather from 'react-native-vector-icons/Feather';
 
 import Home from '../pages/Home';
 import Profile from '../pages/Profile';
@@ -11,10 +12,44 @@ const Tab = createBottomTabNavigator();
 
 function AppRoutes() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Search" component={Search} />
-      <Tab.Screen name="Profile" component={Profile} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarHideOnKeyboard: true,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: '#fff',
+        tabBarStyle: {
+          backgroundColor: '#202225',
+          borderTopWidth: 0,
+        },
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({color, size}) => {
+            return <Feather name="home" color={color} size={size} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({color, size}) => {
+            return <Feather name="search" color={color} size={size} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({color, size}) => {
+            return <Feather name="user" color={color} size={size} />;
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 }
