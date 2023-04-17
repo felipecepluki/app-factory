775222b072431ccaad7cd222b79955f79fbf1913
Felipe Cepluki <felipecepluki@gmail.com>
2023-04-17 04:18:49 -0300

    Adding DevPost functionality: keeping login

--- Devpost/src/contexts/auth.js
+++ Devpost/src/contexts/auth.js
@@ -1,12 +1,26 @@
-import React, {useState, createContext} from 'react';
+import React, {useState, createContext, useEffect} from 'react';
 import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const AuthContext = createContext({});
 
 function AuthProvider({children}) {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
   const [loadingAuth, setLoadingAuth] = useState(false);
+
+  useEffect(() => {
+    async function loadStorage() {
+      const storageUser = await AsyncStorage.getItem('@devapp');
+      if (storageUser) {
+        setUser(JSON.parse(storageUser));
+        setLoading(false);
+      }
+      setLoading(false);
+    }
+    loadStorage();
+  }, []);
 
   async function signUp(email, password, name) {
     setLoadingAuth(true);
@@ -28,6 +42,7 @@ function AuthProvider({children}) {
               email: value.user.email,
             };
             setUser(data);
+            storageUser(data);
             setLoadingAuth(false);
           });
       })
@@ -54,6 +69,7 @@ function AuthProvider({children}) {
           email: value.user.email,
         };
         setUser(data);
+        storageUser(data);
         setLoadingAuth(false);
       })
       .catch(error => {
@@ -62,8 +78,13 @@ function AuthProvider({children}) {
       });
   }
 
+  async function storageUser(data) {
+    await AsyncStorage.setItem('@devapp', JSON.stringify(data));
+  }
+
   return (
-    <AuthContext.Provider value={{signed: !!user, signUp, signIn, loadingAuth}}>
+    <AuthContext.Provider
+      value={{signed: !!user, signUp, signIn, loadingAuth, loading}}>
       {children}
     </AuthContext.Provider>
   );
