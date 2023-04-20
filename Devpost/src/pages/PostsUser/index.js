be1e8ccdffc24446a4cfe6fa5bfbd1c8805802a2
Felipe Cepluki <felipecepluki@gmail.com>
2023-04-20 15:42:51 -0300

    Fetching user posts

--- Devpost/src/pages/PostsUser/index.js
+++ Devpost/src/pages/PostsUser/index.js
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, {useLayoutEffect, useState, useCallback} from 'react';
 import {View, Text} from 'react-native';
+import {
+  useRoute,
+  useNavigation,
+  useFocusEffect,
+} from '@react-navigation/native';
+import firestore from '@react-native-firebase/firestore';
 
 function PostsUser() {
+  const route = useRoute();
+  const navigation = useNavigation();
+  const [title, setTitle] = useState(route.params?.title);
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useLayoutEffect(() => {
+    title: title === '' ? '' : title;
+  }, [navigation, title]);
+
+  useFocusEffect(
+    useCallback(() => {
+      let isActive = true;
+      firestore()
+        .collection('posts')
+        .where('userId', '==', route.params?.userId)
+        .orderBy('created', 'desc')
+        .get()
+        .then(snapshot => {
+          const postList = [];
+          snapshot.docs.map(u => {
+            postList.push({
+              ...u.data(),
+              id: u.id,
+            });
+          });
+          if (isActive) {
+            setPosts(postList);
+            console.log(postList);
+            setLoading(false);
+          }
+        });
+      return () => {
+        isActive = false;
+      };
+    }, []),
+  );
+
   return (
     <View>
-      <Text>PostsUser</Text>
+      <Text>{route.params?.title}</Text>
     </View>
   );
 }
