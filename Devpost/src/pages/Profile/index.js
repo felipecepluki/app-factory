c765fb502a4f83f71f50e83fab025cfdc5bf0523
Felipe Cepluki <felipecepluki@gmail.com>
2023-04-22 16:35:18 -0300

    Create Profile Modal

--- Devpost/src/pages/Profile/index.js
+++ Devpost/src/pages/Profile/index.js
@@ -1,4 +1,5 @@
 import React, {useContext, useState} from 'react';
+import {Modal, Platform} from 'react-native';
 import {AuthContext} from '../../contexts/auth';
 import Header from '../../components/Header';
 import {
@@ -10,15 +11,24 @@ import {
   UploadButton,
   UploadText,
   Avatar,
+  ModalContainer,
+  ButtonBack,
+  Input,
 } from './styles';
+import Feather from 'react-native-vector-icons/Feather';
 
 function Profile() {
   const {signOut, user} = useContext(AuthContext);
   const [nome, setNome] = useState(user?.nome);
   const [url, setUrl] = useState(null);
+  const [open, setOpen] = useState(false);
 
   async function handleSignOut() {
     await signOut();
+  }
+
+  async function updateProfile() {
+    alert('TESTE');
   }
 
   return (
@@ -36,12 +46,28 @@ function Profile() {
       )}
       <Name>{user?.nome}</Name>
       <Email>{user?.email}</Email>
-      <Button bg="#428cfd">
+      <Button bg="#428cfd" onPress={() => setOpen(true)}>
         <ButtonText color="#fff">Atualizar Perfil</ButtonText>
       </Button>
       <Button bg="#ddd" onPress={handleSignOut}>
         <ButtonText color="#353840">Sair</ButtonText>
       </Button>
+      <Modal visible={open} animationType="slide" transparent={true}>
+        <ModalContainer behavior={Platform.OS === 'android' ? '' : 'padding'}>
+          <ButtonBack onPress={() => setOpen(false)}>
+            <Feather name="arrow-left" size={22} color="#121212" />
+            <ButtonText color="#121212">Voltar</ButtonText>
+          </ButtonBack>
+          <Input
+            placeholder={user?.nome}
+            value={nome}
+            onChangeText={text => setNome(text)}
+          />
+          <Button bg="#428cfd" onPress={updateProfile}>
+            <ButtonText color="#fff">Salvar</ButtonText>
+          </Button>
+        </ModalContainer>
+      </Modal>
     </Container>
   );
 }
--- Devpost/src/pages/Profile/styles.js
+++ Devpost/src/pages/Profile/styles.js
@@ -62,3 +62,31 @@ export const Avatar = styled.Image`
   height: 160px;
   border-radius: 80px;
 `;
+
+export const ModalContainer = styled.KeyBoardAvoidingView`
+  width: 100%;
+  height: 70%;
+  background-color: #fff;
+  position: absolute;
+  bottom: 0;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ButtonBack = styled.TouchableOpacity`
+  position: absolute;
+  top: 15px;
+  left: 25px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Input = styled.TextInput`
+  background-color: #ddd;
+  width: 90%;
+  border-radius: 4px;
+  padding: 10px;
+  font-size: 18px;
+  color: #121212;
+  text-align: center;
+`;
