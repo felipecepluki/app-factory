11c5f3432e21c28bf107f47087f3ea3e5358e8c2
Felipe Cepluki <felipecepluki@gmail.com>
2023-04-22 16:17:30 -0300

    Create Profile Page

--- Devpost/src/pages/Profile/index.js
+++ Devpost/src/pages/Profile/index.js
@@ -1,19 +1,48 @@
-import React, {useContext} from 'react';
-import {View, Text, Button} from 'react-native';
+import React, {useContext, useState} from 'react';
 import {AuthContext} from '../../contexts/auth';
+import Header from '../../components/Header';
+import {
+  Container,
+  Name,
+  Email,
+  Button,
+  ButtonText,
+  UploadButton,
+  UploadText,
+  Avatar,
+} from './styles';
 
 function Profile() {
-  const {signOut} = useContext(AuthContext);
+  const {signOut, user} = useContext(AuthContext);
+  const [nome, setNome] = useState(user?.nome);
+  const [url, setUrl] = useState(null);
 
   async function handleSignOut() {
     await signOut();
   }
 
   return (
-    <View>
-      <Text>Profile</Text>
-      <Button title="Sair" onPress={handleSignOut} />
-    </View>
+    <Container>
+      <Header />
+      {url ? (
+        <UploadButton onPress={() => alert('CLICOU')}>
+          <UploadText>+</UploadText>
+          <Avatar source={{uri: url}} />
+        </UploadButton>
+      ) : (
+        <UploadButton onPress={() => alert('CLICOU')}>
+          <UploadText>+</UploadText>
+        </UploadButton>
+      )}
+      <Name>{user?.nome}</Name>
+      <Email>{user?.email}</Email>
+      <Button bg="#428cfd">
+        <ButtonText color="#fff">Atualizar Perfil</ButtonText>
+      </Button>
+      <Button bg="#ddd" onPress={handleSignOut}>
+        <ButtonText color="#353840">Sair</ButtonText>
+      </Button>
+    </Container>
   );
 }
 
