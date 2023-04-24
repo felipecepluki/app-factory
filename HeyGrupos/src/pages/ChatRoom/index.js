970b60ce3882ffdb79e624b433461885150da1e9
Felipe Cepluki <felipecepluki@gmail.com>
2023-04-24 12:24:01 -0300

    Create Login Page

--- HeyGrupos/src/pages/ChatRoom/index.js
+++ HeyGrupos/src/pages/ChatRoom/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native/types';
+import {View, Text, StyleSheet, Button} from 'react-native/types';
 import auth from '@react-native-firebase/auth';
+import {useNavigation} from '@react-navigation/native';
 
 export default function ChatRoom() {
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
       <Text>Tela ChatRoom</Text>
+      <Button title="Login" onPress={() => navigation.navigate('SignIn')} />
     </View>
   );
 }
