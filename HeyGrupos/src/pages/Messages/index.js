20b2146824a12182bf3442e705a96959ef7225e1
Felipe Cepluki <felipecepluki@gmail.com>
2023-05-02 09:49:47 -0300

    feat(chat): building chat

--- HeyGrupos/src/pages/Messages/index.js
+++ HeyGrupos/src/pages/Messages/index.js
@@ -1,8 +1,42 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native/types';
+import React, {useState, useEffect} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 
 export default function Messages({route}) {
   const {thread} = route.params;
+  const [messages, setMessages] = useState([]);
+  const user = auth().currentUser.toJSON();
+
+  useEffect(() => {
+    const unsubscribeListener = firestore()
+      .collection('MESSAGE_THREADS')
+      .doc(thread._id)
+      .collection('MESSAGES')
+      .orderBy('createdAt', 'desc')
+      .onSnapshot(querySnapshot => {
+        const messages = querySnapshot.docs.map(doc => {
+          const firebaseData = doc.data();
+          const data = {
+            _id: doc.id,
+            text: '',
+            createdAt: firestore.FieldValue.serverTimestamp(),
+            ...firebaseData,
+          };
+          if (!firebaseData.system) {
+            data.user = {
+              ...firebaseData.user,
+              name: firebaseData.user.displayName,
+            };
+          }
+          return data;
+        });
+        setMessages(messages);
+      });
+    return () => {
+      unsubscribeListener();
+    };
+  }, []);
 
   return (
     <View style={styles.container}>
