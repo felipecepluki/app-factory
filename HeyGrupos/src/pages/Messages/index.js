4913556ac1a8b2bede75e94f0f502242c8e694fb
Felipe Cepluki <felipecepluki@gmail.com>
2023-05-03 09:30:16 -0300

    feat(message): creating message entry

--- HeyGrupos/src/pages/Messages/index.js
+++ HeyGrupos/src/pages/Messages/index.js
@@ -1,12 +1,24 @@
 import React, {useState, useEffect} from 'react';
-import {View, SafeAreaView, Text, StyleSheet, FlatList} from 'react-native';
+import {
+  View,
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  FlatList,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
 import ChatMessage from '../../components/ChatMessage';
+import Feather from 'react-native-vector-icons/Feather';
 
 export default function Messages({route}) {
   const {thread} = route.params;
   const [messages, setMessages] = useState([]);
+  const [input, setInput] = useState('');
   const user = auth().currentUser.toJSON();
 
   useEffect(() => {
@@ -47,6 +59,28 @@ export default function Messages({route}) {
         keyExtractor={item => item._id}
         renderItem={({item}) => <ChatMessage data={item} />}
       />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'paddding' : 'height'}
+        style={{width: '100%'}}
+        keyboardVerticalOffset={100}>
+        <View style={styles.containerInput}>
+          <View style={styles.mainContainerInput}>
+            <TextInput
+              placeholder="Sua mensagem..."
+              style={styles.textInput}
+              value={input}
+              onChangeText={text => setInput(text)}
+              multiline={true}
+              autoCorrect={false}
+            />
+          </View>
+          <TouchableOpacity>
+            <View style={styles.buttonContainer}>
+              <Feather name="send" size={22} color="#fff" />
+            </View>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
@@ -56,5 +90,32 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  containerInput: {
+    flexDirection: 'row',
+    margin: 10,
+    alignItems: 'flex-end',
+  },
+  mainContainerInput: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    flex: 1,
+    borderRadius: 25,
+    marginRight: 10,
+  },
+  textInput: {
+    flex: 1,
+    marginHorizontal: 10,
+    maxHeight: 130,
+    minHeight: 48,
+  },
+  buttonContainer: {
+    backgroundColor: '#51c880',
+    height: 48,
+    width: 48,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 25,
   },
 });
