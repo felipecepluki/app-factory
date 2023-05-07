3ed8cfc507ae8d85236583a8fb0c9cb92845f5ce
Felipe Cepluki <felipecepluki@gmail.com>
2023-05-07 13:31:13 -0300

    docs(maps): add maps to the README file

--- maps/App.js
+++ maps/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import MapView from "react-native-maps";
-import { StyleSheet, View } from "react-native";
+import { Button, StyleSheet, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View style={{ flexDirection: "row" }}>
+        <Button title="Brasilia" onPress={() => {}} />
+        <Button title="Sao Paulo" onPress={() => {}} />
+      </View>
       <MapView
-        style={{ width: 350, height: 350 }}
-        initialRegion={{
+        style={styles.map}
+        region={{
           latitude: -23.5492243,
           longitude: -46.5813785,
           latitudeDelta: 0.0922,
@@ -23,5 +27,9 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  map: {
+    width: "100%",
+    height: 500,
   },
 });
