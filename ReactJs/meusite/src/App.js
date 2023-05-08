f0cffcf81662a07da9ead7b0137bc0788de87d43
Felipe Cepluki <felipecepluki@gmail.com>
2023-05-08 14:48:07 -0300

    feat(props): understand props and components in React.Js Part 2

--- ReactJs/meusite/src/App.js
+++ ReactJs/meusite/src/App.js
@@ -1,20 +1,46 @@
 import React from "react";
 
-const BemVindo = (props) => (
-  <div>
-    <h2>Bem-vindo(a) {props.nome}</h2>
-    <h3>Tenho {props.idade} anos</h3>
-  </div>
-);
+const Equipe = (props) => {
+  return (
+    <div>
+      <Sobre nome={props.nome} cargo={props.cargo} idade={props.idade} />
+      <Social fb={props.facebook} />
+      <hr />
+    </div>
+  );
+};
+
+const Sobre = (props) => {
+  return (
+    <div>
+      <h2>Olá, sou o(a) {props.nome}</h2>
+      <h3>Cargo: {props.cargo}</h3>
+      <h3>Idade: {props.idade}</h3>
+    </div>
+  );
+};
+
+const Social = (props) => {
+  return (
+    <div>
+      <a href={props.fb}>Facebook</a>
+      <a>Linkedin</a>
+      <a>Youtube</a>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <h1>
-        Olá Mundo!
-        <BemVindo nome="Felipe" idade="18" />
-        <BemVindo nome="Rafael" idade="16" />
-      </h1>
+      <h1>Conheça nossa equipe:</h1>
+      <Equipe
+        nome="Felipe"
+        cargo="Programador"
+        idade="18"
+        facebook="https://google.com"
+      />
+      <Equipe nome="João" cargo="Designer" idade="30" />
     </div>
   );
 }
