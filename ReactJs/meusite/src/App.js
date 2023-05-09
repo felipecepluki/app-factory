c08a267ec17242be32228c43c4a8795a298c39d8
Felipe Cepluki <felipecepluki@gmail.com>
2023-05-09 11:25:59 -0300

    feat(states): working with states

--- ReactJs/meusite/src/App.js
+++ ReactJs/meusite/src/App.js
@@ -1,39 +1,46 @@
 import React, { Component } from "react";
 
-class Equipe extends Component {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nome: "Felipe",
+      contador: 0,
+    };
+    this.aumentar = this.aumentar.bind(this);
+    this.diminuir = this.diminuir.bind(this);
+  }
+
+  aumentar() {
+    let state = this.state;
+    state.contador += 1;
+    state.nome = "Jose";
+    this.setState(state);
+  }
+
+  diminuir() {
+    let state = this.state;
+    if (state.contador === 0) {
+      alert("Opa, chegou a zero!");
+      return;
+    }
+    state.contador -= 1;
+    this.setState(state);
+  }
+
   render() {
     return (
       <div>
-        <Sobre
-          nome={this.props.nome}
-          cargo={this.props.cargo}
-          idade={this.props.idade}
-        />
-        <hr />
+        <h1>Contador</h1>
+        <h3>
+          {this.state.nome}
+          <button onClick={this.diminuir}> - </button>
+          {this.state.contador}
+        </h3>
+        <button onClick={this.aumentar}> + </button>
       </div>
     );
   }
-}
-
-class Sobre extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Olá, sou o(a) {this.props.nome}</h2>
-        <h2>Cargo: {this.props.cargo}</h2>
-        <h3>Idade: {this.props.idade}</h3>
-      </div>
-    );
-  }
-}
-
-function App() {
-  return (
-    <div>
-      <h1>Conheça nossa equipe:</h1>
-      <Equipe nome="Felipe" cargo="Programador" idade="18" />
-    </div>
-  );
 }
 
 export default App;
