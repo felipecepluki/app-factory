3d3f5ced8dc73b650adf11e67fd7154f2f81c322
Felipe Cepluki <felipecepluki@gmail.com>
2023-05-09 11:38:42 -0300

    feat(components): component life cycle

--- ReactJs/meusite/src/App.js
+++ ReactJs/meusite/src/App.js
@@ -4,40 +4,24 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      nome: "Felipe",
-      contador: 0,
+      hora: "00:00:00",
     };
-    this.aumentar = this.aumentar.bind(this);
-    this.diminuir = this.diminuir.bind(this);
   }
 
-  aumentar() {
-    let state = this.state;
-    state.contador += 1;
-    state.nome = "Jose";
-    this.setState(state);
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ hora: new Date().toLocaleTimeString() });
+    }, 1000);
   }
 
-  diminuir() {
-    let state = this.state;
-    if (state.contador === 0) {
-      alert("Opa, chegou a zero!");
-      return;
-    }
-    state.contador -= 1;
-    this.setState(state);
+  componentDidUpdate() {
+    console.log("Atualizou!!!");
   }
 
   render() {
     return (
       <div>
-        <h1>Contador</h1>
-        <h3>
-          {this.state.nome}
-          <button onClick={this.diminuir}> - </button>
-          {this.state.contador}
-        </h3>
-        <button onClick={this.aumentar}> + </button>
+        <h1>Meu projeto {this.state.hora}</h1>
       </div>
     );
   }
