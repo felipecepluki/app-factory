b8512b0694639353c59376f3a19ec53e75cdfa82
Felipe Cepluki <felipecepluki@gmail.com>
2023-05-09 11:13:15 -0300

    feat(class): applying Class Component

--- ReactJs/meusite/src/App.js
+++ ReactJs/meusite/src/App.js
@@ -1,46 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Equipe = (props) => {
-  return (
-    <div>
-      <Sobre nome={props.nome} cargo={props.cargo} idade={props.idade} />
-      <Social fb={props.facebook} />
-      <hr />
-    </div>
-  );
-};
+class Equipe extends Component {
+  render() {
+    return (
+      <div>
+        <Sobre
+          nome={this.props.nome}
+          cargo={this.props.cargo}
+          idade={this.props.idade}
+        />
+        <hr />
+      </div>
+    );
+  }
+}
 
-const Sobre = (props) => {
-  return (
-    <div>
-      <h2>Olá, sou o(a) {props.nome}</h2>
-      <h3>Cargo: {props.cargo}</h3>
-      <h3>Idade: {props.idade}</h3>
-    </div>
-  );
-};
-
-const Social = (props) => {
-  return (
-    <div>
-      <a href={props.fb}>Facebook</a>
-      <a>Linkedin</a>
-      <a>Youtube</a>
-    </div>
-  );
-};
+class Sobre extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Olá, sou o(a) {this.props.nome}</h2>
+        <h2>Cargo: {this.props.cargo}</h2>
+        <h3>Idade: {this.props.idade}</h3>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div>
       <h1>Conheça nossa equipe:</h1>
-      <Equipe
-        nome="Felipe"
-        cargo="Programador"
-        idade="18"
-        facebook="https://google.com"
-      />
-      <Equipe nome="João" cargo="Designer" idade="30" />
+      <Equipe nome="Felipe" cargo="Programador" idade="18" />
     </div>
   );
 }
