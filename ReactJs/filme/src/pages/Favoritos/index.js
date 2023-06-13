bfbf410b1fe7ca86096836193bb37876a9cf7482
Felipe Cepluki <felipecepluki@gmail.com>
2023-06-13 15:04:39 -0300

    feat(film): excluding movies

--- ReactJs/filme/src/pages/Favoritos/index.js
+++ ReactJs/filme/src/pages/Favoritos/index.js
@@ -10,9 +10,19 @@ function Favoritos() {
     setFilmes(JSON.parse(minhaLista) || []);
   }, []);
 
+  function excluirFilmes(id) {
+    let filtroFilmes = filmes.filter((item) => {
+      return item.id !== id;
+    });
+
+    setFilmes(filtroFilmes);
+    localStorage.setItem("@primeflix", JSON.stringify(filtroFilmes));
+  }
+
   return (
     <div className="meus-filmes">
       <h1>Meus filmes</h1>
+      {filmes.length === 0 && <span>Você não possui nenhum filme salvo :(</span>}
       <ul>
         {filmes.map((item) => {
           return (
@@ -20,7 +30,7 @@ function Favoritos() {
               <span>{item.title}</span>
               <div>
                 <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
-                <button>Excluir</button>
+                <button onClick={() => excluirFilmes(item.id)}>Excluir</button>
               </div>
             </li>
           )
