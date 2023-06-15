2c6c2c331a5a43045429eefeeb1522acfcf98857
Felipe Cepluki <felipecepluki@gmail.com>
2023-06-15 13:13:54 -0300

    feat(firebase): using database

--- ReactJs/fireapp/src/App.js
+++ ReactJs/fireapp/src/App.js
@@ -1,9 +1,46 @@
+import { useState } from "react";
 import { db } from "./firebaseConnection";
+import { doc, setDoc, collection, addDoc, getDoc } from "firebase/firestore";
+import "./App.css";
 
 function App() {
+  const [titulo, setTitulo] = useState("");
+  const [autor, setAutor] = useState("");
+
+  async function handleAdd() {
+    await addDoc(collection(db, "posts"), {
+      titulo: titulo,
+      autor: autor,
+    })
+    .then(() => {
+      console.log("DADOS CADASTRADOS COM SUCESSO")
+    })
+    .catch((error) => {
+      console.log("ERRO" + error)
+    })
+  }
+
+  async function buscarPost() {
+    const postRef = doc(db, "posts", "12345")
+    await getDoc(postRef)
+    .then((snapshot) => {
+      setAutor(snapshot.data().autor)
+      setTitulo(snapshot.data().titulo)
+    })
+    .catch(() => console.log("ERRO AO BUSCAR"))
+  }
+
   return (
     <div className="App">
       <h1>ReactJs + Firebase :)</h1>
+      <div className="container">
+        <label>Título:</label>
+        <textarea type="text" placeholder="Digite o título" value={titulo} onChange={(e) => setTitulo(e.target.value)} />
+        <label>Autor:</label>
+        <input type="text" placeholder="Autor do post" value={autor} onChange={(e) => setAutor(e.target.value)} />
+        <button onClick={handleAdd}>Cadastrar</button>
+        <button onClick={buscarPost}>Buscar post</button>
+      </div>
     </div>
   );
 }
