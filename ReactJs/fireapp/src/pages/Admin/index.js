d97d19c22412312db9251553baebfdb44f60ea07
Felipe Cepluki <felipecepluki@gmail.com>
2023-06-21 14:39:41 -0300

    feat(admin): create admin page

--- ReactJs/fireapp/src/pages/Admin/index.js
+++ ReactJs/fireapp/src/pages/Admin/index.js
@@ -1,9 +1,38 @@
+import { useState } from "react";
 import "./admin.css";
+import { auth } from '../../firebaseConnection'
+import { signOut } from "firebase/auth"
 
 export function Admin() {
+  const [tarefaInput, setTarefaInput] = useState('')
+
+  function handleRegister(e) {
+    e.preventDefault()
+  }
+
+  async function handleLogout() {
+    await signOut(auth);
+  }
+
   return (
-    <div>
-      <h1>Admin</h1>
+    <div className="admin-container">
+      <h1>Minhas tarefas</h1>
+      <form onSubmit={handleRegister}>
+        <textarea 
+          placeholder="Digite sua tarefa..." 
+          value={tarefaInput} 
+          onChange={(e) => setTarefaInput(e.target.value)} 
+        />
+        <button className="btn-register" type="submit">Registrar tarefa</button>
+      </form>
+      <article className="list">
+        <p>Estudar javascript e react</p>
+        <div>
+          <button>Editar</button>
+          <button className="btn-delete">Concluir</button>
+        </div>
+        <button className="btn-logout" onClick={handleLogout}>Sair</button>
+      </article>
     </div>
   )
 }
