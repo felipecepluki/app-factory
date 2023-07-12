f25c980d3f19fb1bfa78cc35e9972d05e7847f06
Felipe Cepluki <felipecepluki@gmail.com>
2023-07-12 14:24:13 -0300

    feat(system): create the login page

--- ReactJs/system/src/pages/SignIn/index.js
+++ ReactJs/system/src/pages/SignIn/index.js
@@ -1,7 +1,36 @@
+import { useState } from "react"
+import "./signin.css"
+import logo from "../../assets/logo.png"
+import { Link } from "react-router-dom";
+
 export default function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div>
-      <h1>Page SignIn</h1>
+    <div className="container-center">
+      <div className="login">
+        <div className="login-area">
+          <img src={logo} alt="Logo of the system" />
+        </div>
+        <form>
+          <h1>Entrar</h1>
+          <input 
+            type="text"
+            placeholder="email@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input 
+            type="password"
+            placeholder="*********"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Acessar</button>
+        </form>
+        <Link to="/register">Criar uma conta</Link>
+      </div>
     </div>
   )
 }
